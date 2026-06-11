313f30892621cb19c08da9ea312427a8ee300569
Ulziidemberel <ulziidemberel@deutschewelle.mn>
2026-06-11 13:51:15 +0800

    Add fallback URLs for env vars

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -1,13 +1,16 @@
 import { HttpLink, ApolloClient, InMemoryCache } from "@apollo/client";
 import { registerApolloClient } from "@apollo/client-integration-nextjs";
 
+const ERXES_API_URL = process.env.ERXES_API_URL || "https://ulziidemberel.next.erxes.io/gateway/graphql";
+const ERXES_APP_TOKEN = process.env.ERXES_APP_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRQb3J0YWxJZCI6InlUSE9Fem11ZVkxbmlZYkZiMDNyNiIsImlhdCI6MTc4MTAwNDMyNn0.8RDQVrd50HWr1fTMUO67LAfoMXnESE59TEq7lkVDZnQ";
+
 export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      uri: process.env.ERXES_API_URL,
+      uri: ERXES_API_URL,
       headers: {
-        "x-app-token": process.env.ERXES_APP_TOKEN ?? "",
+        "x-app-token": ERXES_APP_TOKEN,
       },
     }),
   });
--- src/lib/apollo-wrapper.tsx
+++ src/lib/apollo-wrapper.tsx
@@ -3,13 +3,16 @@
 import { HttpLink } from "@apollo/client";
 import { ApolloNextAppProvider, ApolloClient, InMemoryCache } from "@apollo/client-integration-nextjs";
 
+const NEXT_PUBLIC_ERXES_API_URL = process.env.NEXT_PUBLIC_ERXES_API_URL || "https://ulziidemberel.next.erxes.io/gateway/graphql";
+const NEXT_PUBLIC_ERXES_APP_TOKEN = process.env.NEXT_PUBLIC_ERXES_APP_TOKEN || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRQb3J0YWxJZCI6InlUSE9Fem11ZVkxbmlZYkZiMDNyNiIsImlhdCI6MTc4MTAwNDMyNn0.8RDQVrd50HWr1fTMUO67LAfoMXnESE59TEq7lkVDZnQ";
+
 function makeClient() {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-      uri: process.env.NEXT_PUBLIC_ERXES_API_URL,
+      uri: NEXT_PUBLIC_ERXES_API_URL,
       headers: {
-        "x-app-token": process.env.NEXT_PUBLIC_ERXES_APP_TOKEN ?? "",
+        "x-app-token": NEXT_PUBLIC_ERXES_APP_TOKEN,
       },
     }),
   });
